4c84e9b28c6d4991efcc759ea1ed711813c6b557
Theoxenos <theoxenos@pm.me>
2026-04-09 14:47:25 +0200

    Implement basic get endpoints for all models

---
+++ GhibliApiNet.Api/Models/Domain/Location.cs
@@ -0,0 +1,12 @@
+namespace GhibliApiNet.Api.Models.Domain;
+
+public class Location
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Climate { get; set; }
+    public string? Terrain { get; set; }
+    public int? SurfaceWater { get; set; }
+    public List<Person>? Residents { get; set; }
+    public List<Film>? Films { get; set; }
+}
---
+++ GhibliApiNet.Api/Models/Domain/Species.cs
@@ -0,0 +1,12 @@
+namespace GhibliApiNet.Api.Models.Domain;
+
+public class Species
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Classification { get; set; }
+    public string? EyeColors { get; set; }
+    public string? HairColors { get; set; }
+    public List<Person>? People { get; set; }
+    public List<Film>? Films { get; set; }
+}
